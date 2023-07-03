d1b70993b870b8dc33acbaf8d5a9cd20f714cbf2
SahibMobDev <sahibmobdev@gmail.com>
2023-07-03 23:00:45 +0400

    Show several queries from SearchView on screen

--- app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryFragment.kt
@@ -34,6 +34,8 @@ class PhotoGalleryFragment : Fragment() {
             "Cannot access binding because it is null. Is the view visible?"
         }
 
+    private var searchView: SearchView? = null
+
     private val photoGalleryViewModel: PhotoGalleryViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,8 +58,9 @@ class PhotoGalleryFragment : Fragment() {
 
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                photoGalleryViewModel.galleryItem.collect {items ->
-                    binding.photoGrid.adapter = PhotoListAdapter(items)
+                photoGalleryViewModel.uiState.collect {state ->
+                    binding.photoGrid.adapter = PhotoListAdapter(state.images)
+                    searchView?.setQuery(state.query, false)
                 }
             }
         }
@@ -73,7 +76,7 @@ class PhotoGalleryFragment : Fragment() {
         inflater.inflate(R.menu.fragment_photo_gallery, menu)
 
         val searchItem: MenuItem = menu.findItem(R.id.menu_item_search)
-        val searchView = searchItem.actionView as? SearchView
+        searchView = searchItem.actionView as? SearchView
 
         searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
             override fun onQueryTextSubmit(query: String?): Boolean {
@@ -98,5 +101,10 @@ class PhotoGalleryFragment : Fragment() {
                 true
             } else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    override fun onDestroyOptionsMenu() {
+        super.onDestroyOptionsMenu()
+        searchView = null
     }
 }
--- app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
+++ app/src/main/java/com/example/photogallery/PhotoGalleryViewModel.kt
@@ -7,6 +7,7 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
 private const val TAG = "PhotoGalleryViewModel"
@@ -15,16 +16,26 @@ class PhotoGalleryViewModel : ViewModel() {
     private val photoRepository = PhotoRepository()
     private val preferencesRepository = PreferencesRepository.get()
 
-    private val _galleryItems: MutableStateFlow<List<GalleryItem>> = MutableStateFlow(emptyList())
+/*    private val _galleryItems: MutableStateFlow<List<GalleryItem>> = MutableStateFlow(emptyList())
     val galleryItem: StateFlow<List<GalleryItem>>
-        get() = _galleryItems.asStateFlow()
+        get() = _galleryItems.asStateFlow()*/
+
+    private val _uiState: MutableStateFlow<PhotoGalleryUiState> = MutableStateFlow(PhotoGalleryUiState())
+    val uiState: StateFlow<PhotoGalleryUiState>
+        get() = _uiState.asStateFlow()
 
     init {
         viewModelScope.launch {
             preferencesRepository.storedQuery.collectLatest { storedQuery ->
                 try {
-                    val items = photoRepository.searchPhotos(storedQuery)
-                    _galleryItems.value = items
+                    //val items = photoRepository.searchPhotos(storedQuery)
+                    val items = fetchGalleryItems(storedQuery)
+                    _uiState.update { oldState ->
+                        oldState.copy(
+                            images = items,
+                            query = storedQuery
+                        )
+                    }
                 } catch (ex: Exception) {
                     Log.d(TAG, "Failed to fetch gallery items", ex)
                 }
@@ -44,3 +55,8 @@ class PhotoGalleryViewModel : ViewModel() {
         }
     }
 }
+
+data class PhotoGalleryUiState(
+    val images: List<GalleryItem> = listOf(),
+    val query: String = ""
+)
